fd4ee058cfdc3ce39a27dd473d1de3831d194a1e
Raclett3 <im@s283.net>
2021-02-18 11:46:44 +0900

    Add usize_bound

--- src/bound.rs
+++ src/bound.rs
@@ -15,6 +15,16 @@ pub fn bound<F: Fn(isize) -> bool>(min: isize, max: isize, f: F) -> isize {
     ok
 }
 
+#[snippet("bound")]
+pub fn usize_bound<F: Fn(usize) -> bool>(min: usize, max: usize, f: F) -> usize {
+    let exclusive_max = if min <= max {
+        max as isize + 1
+    } else {
+        max as isize - 1
+    };
+    bound(min as isize, exclusive_max, |x| f(x as usize)) as usize
+}
+
 #[test]
 fn test_bound() {
     assert_eq!(bound(0, 101, |x| x <= 10), 10);
@@ -23,4 +33,7 @@ fn test_bound() {
     assert_eq!(bound(100, -1, |x| x >= 20), 20);
     assert_eq!(bound(100, -1, |x| x >= 0), 0);
     assert_eq!(bound(100, -1, |x| x >= -1), 0);
+    assert_eq!(usize_bound(0, 100, |x| x <= 10), 10);
+    assert_eq!(usize_bound(100, 0, |x| x >= 30), 30);
+    assert_eq!(usize_bound(100, 10, |x| x >= 5), 10);
 }

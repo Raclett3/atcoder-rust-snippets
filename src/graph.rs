c07625295cfe5bbed47225bf907755455d461fbf
Raclett3 <im@s283.net>
2021-04-02 15:10:47 +0900

    Better Graph

--- src/graph.rs
+++ src/graph.rs
@@ -8,24 +8,10 @@ use std::cmp::*;
 use std::collections::binary_heap::BinaryHeap;
 
 #[snippet("graph")]
-#[derive(Copy, Clone, Eq, PartialEq)]
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
 pub struct GraphEdge {
-    node_to: usize,
     cost: isize,
-}
-
-#[snippet("graph")]
-impl Ord for GraphEdge {
-    fn cmp(&self, other: &GraphEdge) -> Ordering {
-        other.cost.cmp(&self.cost)
-    }
-}
-
-#[snippet("graph")]
-impl PartialOrd for GraphEdge {
-    fn partial_cmp(&self, other: &GraphEdge) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
+    node_to: usize,
 }
 
 #[snippet("graph")]
@@ -68,39 +54,44 @@ impl Graph {
         self.edges[node_from].push(GraphEdge::new(node_to, cost));
     }
 
-    pub fn dijkstra(&self, node_from: usize) -> Vec<isize> {
-        let mut costs = vec![std::isize::MAX; self.nodes];
-        let mut heap: BinaryHeap<GraphEdge> = BinaryHeap::new();
-        heap.push(GraphEdge::new(node_from, 0));
-        while let Some(GraphEdge { cost, node_to }) = heap.pop() {
-            if cost >= costs[node_to] {
+    pub fn dijkstra(&self, node_from: usize) -> Vec<Option<isize>> {
+        let mut costs = vec![None; self.nodes];
+        let mut heap: BinaryHeap<Reverse<GraphEdge>> = BinaryHeap::new();
+        heap.push(Reverse(GraphEdge::new(node_from, 0)));
+        while let Some(Reverse(GraphEdge { cost, node_to })) = heap.pop() {
+            if cost >= costs[node_to].unwrap_or(std::isize::MAX) {
                 continue;
             }
-            costs[node_to] = cost;
+            costs[node_to] = Some(cost);
 
             for edge in &self.edges[node_to] {
                 let next = GraphEdge::new(edge.node_to, cost + edge.cost);
-                heap.push(next);
+                heap.push(Reverse(next));
             }
         }
         costs
     }
 
-    pub fn warshall_floyd(&self) -> Vec<Vec<isize>> {
-        let mut costs = vec![vec![std::isize::MAX; self.nodes]; self.nodes];
+    pub fn warshall_floyd(&self) -> Vec<Vec<Option<isize>>> {
+        let mut costs = vec![vec![None; self.nodes]; self.nodes];
         for i in 0..self.nodes {
-            costs[i][i] = 0;
+            costs[i][i] = Some(0);
         }
         for (node_from, edges) in self.edges.iter().enumerate() {
             for GraphEdge { node_to, cost } in edges.iter() {
-                costs[node_from][*node_to] = *cost;
+                costs[node_from][*node_to] = Some(*cost);
             }
         }
         for k in 0..self.nodes {
             for i in 0..self.nodes {
                 for j in 0..self.nodes {
-                    costs[i][j] =
-                        std::cmp::min(costs[i][j], costs[i][k].saturating_add(costs[k][j]));
+                    costs[i][j] = match (costs[i][j], (costs[i][k], costs[k][j])) {
+                        (Some(cost), (Some(cost_ik), Some(cost_kj))) => {
+                            Some(cost.min(cost_ik + cost_kj))
+                        }
+                        (None, (Some(cost_ik), Some(cost_kj))) => Some(cost_ik + cost_kj),
+                        (cost, _) => cost,
+                    };
                 }
             }
         }
@@ -124,7 +115,19 @@ fn test_graph() {
     graph.edge_undirected_costed(2, 6, 4);
     graph.edge_undirected_costed(3, 6, 6);
     graph.edge_undirected_costed(5, 6, 5);
-    assert_eq!(vec![0, 6, 19, 9, 12, 12, 15, 2], graph.dijkstra(0));
+    assert_eq!(
+        vec![
+            Some(0),
+            Some(6),
+            Some(19),
+            Some(9),
+            Some(12),
+            Some(12),
+            Some(15),
+            Some(2)
+        ],
+        graph.dijkstra(0)
+    );
     let mut graph = Graph::new(6);
     graph.edge_undirected_costed(0, 1, 5);
     graph.edge_undirected_costed(0, 4, 15);
@@ -132,14 +135,13 @@ fn test_graph() {
     graph.edge_undirected_costed(2, 3, 2);
     graph.edge_undirected_costed(2, 4, 9);
     graph.edge_undirected_costed(3, 4, 6);
-    let inf = std::isize::MAX;
     let expected = vec![
-        vec![0, 5, 6, 8, 14, inf],
-        vec![5, 0, 1, 3, 9, inf],
-        vec![6, 1, 0, 2, 8, inf],
-        vec![8, 3, 2, 0, 6, inf],
-        vec![14, 9, 8, 6, 0, inf],
-        vec![inf, inf, inf, inf, inf, 0],
+        vec![Some(0), Some(5), Some(6), Some(8), Some(14), None],
+        vec![Some(5), Some(0), Some(1), Some(3), Some(9), None],
+        vec![Some(6), Some(1), Some(0), Some(2), Some(8), None],
+        vec![Some(8), Some(3), Some(2), Some(0), Some(6), None],
+        vec![Some(14), Some(9), Some(8), Some(6), Some(0), None],
+        vec![None, None, None, None, None, Some(0)],
     ];
     assert_eq!(expected, graph.warshall_floyd());
 }
